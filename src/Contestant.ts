4d8e95e3a573e4dba931fcc95dda36f5e215c17a
[bokamatyas] <[boka.matyas@students.jedlik.eu]>
2024-11-15 11:20:39 +0100

    Adding basic/required getters

--- src/Contestant.ts
+++ src/Contestant.ts
@@ -1,28 +1,50 @@
 export default class Contestant{
 
-    #name: string;
-    #group: string;
-    #nation: string;
-    #natCode: string;
-    #throws: number[];
+    #_name: string;
+    #_group: string;
+    #_nationAndCode: string;
+    #_throws: number[];
+
+    get name(): string{
+        return this.#_name;
+    }
+
+    get group(): string{
+        return this.#_group;
+    }
+
+    private get nationAndCode(): string[]{
+        return this.#_nationAndCode.split(' ');
+    }
+
+    get nation(): string{
+        return this.nationAndCode[0];
+    }
+
+    get natCode(): string{
+        let codeRaw: string = this.nationAndCode[1];
+        return codeRaw.substring(1, codeRaw.length-1);
+    }
+
+    get throws(): number[]{
+        return this.#_throws;
+    }
 
     constructor(row:string){
         let data: string[] = row.split(';');
-        this.#name = data[0];
-        this.#group = data[1];
+        this.#_name = data[0];
+        this.#_group = data[1];
 
-        let nationSplitted: string[] = data[2].split(' ');
-        this.#nation = nationSplitted[0];
-        this.#natCode = nationSplitted[1].substring(1, nationSplitted[1].length-1);
+        this.#_nationAndCode = data[2];
 
-        this.#throws = [];
+        this.#_throws = [];
         for (let i = 3; i <= 5; i++) {            
             if(data[i] == 'X')
-                this.#throws.push(-1.0);
+                this.#_throws.push(-1.0);
             if(data[i] == '-')
-                this.#throws.push(-2.0);
+                this.#_throws.push(-2.0);
             else
-                this.#throws.push(+(data[i].replace(',', '.')));
+                this.#_throws.push(+(data[i].replace(',', '.')));
         }
     }
 }
