b9c4e7ead217e6f591077c56a5775d4814a14699
mateszadam <matesz.adam@gmail.com>
2024-11-25 11:10:06 +0100

    update Contestant tests to include formattedOutput and qualification checks

--- src/__tests__/Contestant.test.ts
+++ src/__tests__/Contestant.test.ts
@@ -4,14 +4,30 @@ describe("Tests for the Contestant class", () => {
   let contestant: Contestant;
   beforeEach(() => {
     contestant = new Contestant(
-      "A. G. Kruger;B;Egyesült Államok (USA);X;72,13;X"
+      "A. G. Kruger;B;Egyesült Államok (USA);X;72,13;-"
     );
   });
   it("should getters return good value", () => {
     expect(contestant.name).toBe("A. G. Kruger");
-    expect(contestant.throws).toEqual([-1, 72.13, -1]);
+    expect(contestant.throws).toEqual([-1, 72.13, -2]);
     expect(contestant.nation).toBe("Egyesült Államok");
     expect(contestant.natCode).toBe("USA");
     expect(contestant.group).toBe("B");
+  });
+  it("should formattedOutput return good value", () => {
+    expect(contestant.formattedOutput).toBe(
+      `Név: A. G. Kruger
+    \tCsoport: B
+    \tNemzet: Egyesült Államok
+    \tNemzet kód: USA
+    \tSorozat: X;72,13;-
+    \tEredmény: 72,13\n`
+    );
+  });
+  it("should isQualified return good value", () => {
+    expect(contestant.isQualified).toBe(false);
+  });
+  it("should result return good value", () => {
+    expect(contestant.result).toBe(72.13);
   });
 });
