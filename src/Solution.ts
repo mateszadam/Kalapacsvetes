a5a62115d3b852eb79ffc6d33d619ce3a7a991e4
[bokamatyas] <[boka.matyas@students.jedlik.eu]>
2024-11-25 10:23:44 +0100

    Formatting output

--- src/Solution.ts
+++ src/Solution.ts
@@ -25,9 +25,9 @@ export default class Solution {
   #writeFile(_fileName: string): void {        
     try {      
       let top12: Contestant[] = this.#contestants.sort((a, b) => (b.result - a.result));         
-      let out: string = "Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény\n\r";
-      top12.forEach((c, i) =>{
-        out+=`${i};${c.name};${c.group};${c.nation};${c.natCode};${c.throwsString};${c.result}\n\r`;
+      let out: string = "Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény\r";
+      top12.slice(0, 12).forEach((c, i) =>{
+        out+=`${i+1};${c.name};${c.group};${c.nation};${c.natCode};${c.throwsString};${c.result}\r`;
       })
       fs.writeFileSync(_fileName, out);
     } catch (error) {
